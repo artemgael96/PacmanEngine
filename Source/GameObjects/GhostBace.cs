63c5d7a24bed6c7323cc8dd6be55a6071dea0e8a
Artem Gael <50806966+artemgael96@users.noreply.github.com>
2020-01-28 08:41:05 +0300

    updated project
    added blinky object and search pacman

--- Source/GameObjects/GhostBace.cs
+++ Source/GameObjects/GhostBace.cs
@@ -13,39 +13,79 @@ namespace ConsoleApp.Source.GameObjects
 {
     public abstract class GhostBace : GameObject
     {
-        private enum Direction { Up, Down, Left, Right }
-        private enum GhostState { Regular, BlueGhost, Eyes }
+        protected enum Direction { Up, Down, Left, Right }
+        protected enum GhostState { Regular, BlueGhost, Eyes }
 
         private const int RegularGhostSpeed = Coordinate.Multiplier / 8;
         private const int BlueGhostSpeed = Coordinate.Multiplier / 10;
         private const int EyesSpeed = Coordinate.Multiplier / 16;
 
+        private Coordinate step;
 
-        protected Coordinate step;
-        public GhostBace(int x, int y, string name, AnimationType? animationType) : base(x, y, name, animationType)
-        {
-           var loc = ManagerObject.Instance.PacmanLocation;
-        }
+        protected Direction currentDirection = Direction.Down;
+        protected GhostState currentState = GhostState.Regular;
+
+        public GhostBace(int x, int y, string name, AnimationType? animationType) : base(x, y, name, animationType) { }
+
         protected abstract Animation GetAnimation();
         protected abstract Coordinate GetTargetCoordinate(Coordinate PacmanLocation);
+
         public override void Update()
         {
             Animation.Location += step;
-
             if (Animation.Location.isRoundAll())
             {
                 var target = GetTargetCoordinate(ManagerObject.Instance.PacmanLocation);
+
                 var path = PathFinder.GetPath(Animation.Location, target);
 
                 if (path != null && path.Count() > 1)
                 {
                     var moveVector = path[1] - Animation.Location;
-
                     Direction newDirection =
                         moveVector.X > 0 ? Direction.Right :
                         moveVector.X < 0 ? Direction.Left :
                         moveVector.Y > 0 ? Direction.Down : Direction.Up;
+
+                    if (newDirection != currentDirection)
+                    {
+                        currentDirection = newDirection; ;
+                        step = GetStep();
+                        var currentPosition = Animation.Location;
+                        Animation = GetAnimation();
+                        Animation.Location = currentPosition;
+                    }
                 }
+            }
+        }
+        private Coordinate GetStep()
+        {
+            int currentSpeed;
+            switch (currentState)
+            {
+                case GhostState.BlueGhost:
+                    currentSpeed = BlueGhostSpeed;
+                    break;
+                case GhostState.Eyes:
+                    currentSpeed = EyesSpeed;
+                    break;
+                case GhostState.Regular:
+                    currentSpeed = RegularGhostSpeed;
+                    break;
+                default: throw new Exception("Unknown ghost state");
+            }
+
+            switch (currentDirection)
+            {
+                case Direction.Up:
+                    return new Coordinate(0, -currentSpeed);
+                case Direction.Down:
+                    return new Coordinate(0, currentSpeed);
+                case Direction.Left:
+                    return new Coordinate(-currentSpeed, 0);
+                case Direction.Right:
+                    return new Coordinate(currentSpeed, 0);
+                default: throw new Exception("Unknown ghost currentDirectio");
             }
         }
     }
